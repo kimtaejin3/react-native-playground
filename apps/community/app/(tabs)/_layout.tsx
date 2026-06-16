c2a262f96ced82e264ea21ac6d680542e5d1aea3
kimtaejin3 <rlaxowls1316@naver.com>
2026-06-16 22:11:09 +0900

    feat(community): style tab ui

--- apps/community/app/(tabs)/_layout.tsx
+++ apps/community/app/(tabs)/_layout.tsx
@@ -1,24 +1,46 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import { colors } from "../constants";
+import { MaterialIcons } from "@react-native-vector-icons/material-icons";
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "black",
+        tabBarActiveTintColor: colors.ORANGE_600,
         headerShown: false,
+        tabBarStyle: {
+          height: 80,
+          borderRadius: 50,
+          paddingTop: 5,
+        },
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          title: "홈",
+          tabBarIcon: ({ color, focused }) => (
+            <MaterialIcons name="home" size={20} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="my"
         options={{
-          title: "Explore",
+          title: "내 프로필",
+          tabBarIcon: ({ color, focused }) => (
+            <MaterialIcons name="person" size={20} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="setting"
+        options={{
+          title: "설정",
+          tabBarIcon: ({ color, focused }) => (
+            <MaterialIcons name="settings" size={18} color={color} />
+          ),
         }}
       />
     </Tabs>
